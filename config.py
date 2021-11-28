486fc5f747b2d40c9d3d880bdfbf21c86f77bbcf
xfortisfye <52729376+ehandywhyy@users.noreply.github.com>
2021-11-29 01:11:10 +0800

    remove gui

--- config.py
+++
@@ -1,4 +0,0 @@
-NUM_FEATURES = 31
-TIMESTEPS = 1
-DATASET_LINK = "https://raw.githubusercontent.com/ehandywhyy/ict3204-security-analytics/main/dataset/overall.csv"
-TEST_DATA_LINK = "https://raw.githubusercontent.com/ehandywhyy/ict3204-security-analytics/main/dataset/overall_test.csv"
